9f4b4e4caeb46cab744ff44661457d0c5c8a9f05
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-21 23:19:47 +0900

    getting request and checking validity of it and if it is valid checking whether the user is already in database or not (using its email) findOne method, if it doesn't exist then hashing its password and saving the user into database

--- routes/api/users.ts
+++ routes/api/users.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 const express = require("express");
 import { check, validationResult } from "express-validator";
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require("../../models/User");
 
 const router = express.Router();
 
@@ -9,6 +13,7 @@ const router = express.Router();
 // @access         Public
 router.post(
   "/",
+
   [
     check("name", "name shouldn't be empty").not().isEmpty(),
     check("email", "please enter a valid email address").isEmail(),
@@ -17,13 +22,51 @@ router.post(
       "password should be at least 6 characters long."
     ).isLength({ min: 6 }),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("users route");
+
+    const { email, password, name } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = await gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        password,
+        avatar
+      })
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send("users route");
+    } catch (error: any) {
+      console.error(error.message);
+      res.status(500).json("Server error");
+    }
   }
 );
 
