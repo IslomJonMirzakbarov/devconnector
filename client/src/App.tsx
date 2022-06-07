21032c736afecb54a03dcd51abe2319c1a424148
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-06-07 22:08:54 +0900

    setup react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { useLocation, useRoutes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import { authorizedRoutes } from "./routes/authorized";
+
+// create a client
+const queryClient = new QueryClient();
 
 function App() {
   const routes = useRoutes(authorizedRoutes);
   const { pathname } = useLocation();
 
   return (
-    <div className="App">
-      <Navbar />
-      {pathname === "/" ? (
-        routes
-      ) : (
-        <section className="container">{routes}</section>
-      )}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Navbar />
+        {pathname === "/" ? (
+          routes
+        ) : (
+          <section className="container">{routes}</section>
+        )}
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
