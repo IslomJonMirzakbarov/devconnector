8090373a931a98e03857ff41f68899c3e7899a44
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-06-12 18:49:04 +0900

    updated

--- routes/api/auth.ts
+++ routes/api/auth.ts
@@ -12,29 +12,25 @@ const User = require("../../models/User");
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
-router.get('/', auth, async (req: any, res: any) => {
+router.get("/", auth, async (req: any, res: any) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select("-password");
     res.json(user);
   } catch (err: any) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 // @routes         POST api/auth
 // @desc           Authenticate user & get token
 // @access         Public
-
 router.post(
   "/",
 
   [
     check("email", "please enter a valid email address").isEmail(),
-    check(
-      "password",
-      "password is required"
-    ).isLength({ min: 6 }),
+    check("password", "password is required").isLength({ min: 6 }),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -57,7 +53,7 @@ router.post(
 
       const isMatch = await bcrypt.compare(password, user.password);
 
-      if(!isMatch) {
+      if (!isMatch) {
         return res
           .status(400)
           .json({ errors: [{ msg: "Invalid credentials" }] });
