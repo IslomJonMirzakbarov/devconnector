6cbb5be2c9969ad109c1ff3f7ac54301144968fd
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-08-15 18:41:05 +0900

    updated

--- server.ts
+++ server.ts
@@ -1,20 +1,20 @@
-import express, { Application } from "express";
+const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 
-const app: Application = express();
+const app = express();
 
-// connect to mongoatlas
+// Connect Database
 connectDB();
 
-// Init middleware
+// Init Middleware
 app.use(express.json());
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/post"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -28,4 +28,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
